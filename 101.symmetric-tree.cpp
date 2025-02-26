1cf65e600144617d3a80edc7dbebd55728210a9b
Yuxin Yan <yuxin.yan.cs@gmail.com>
2025-02-26 23:15:00 +0800

    feat: finish 101.symmetric-tree.cpp

---
+++ 101.symmetric-tree.cpp
@@ -0,0 +1,35 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool isSymmetric(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+    return isMirror(root->left, root->right);
+  }
+
+private:
+  bool isMirror(TreeNode *left, TreeNode *right) {
+    if (left == nullptr && right == nullptr) {
+      return true;
+    } else if (left == nullptr || right == nullptr || left->val != right->val) {
+      return false;
+    }
+
+    return isMirror(left->left, right->right) &&
+           isMirror(left->right, right->left);
+  }
+};
+// @leet end
