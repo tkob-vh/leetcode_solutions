485d66943a65571531c14b32f8f68110e0c5892c
Yuxin Yan <yuxin.yan.cs@gmail.com>
2025-03-06 23:32:28 +0800

    feat: finish 141.linked-list-cycle.cpp

---
+++ 141.linked-list-cycle.cpp
@@ -0,0 +1,29 @@
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    if (head == nullptr) {
+      return false;
+    }
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (fast->next != nullptr && fast->next->next != nullptr) {
+      fast = fast->next->next;
+      slow = slow->next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+// @leet end
